70e1d10aceead878cf15f8ef25260f98de320680
Leo <lechewu@gmail.com>
2023-04-26 23:03:31 +0800

    feat: add getCycle

--- src/libraries/utils.ts
+++ src/libraries/utils.ts
@@ -4,3 +4,40 @@ export const isLabelValid = (
     title: string
   }[],
 ) => nodes.every(({ title }) => title !== label)
+
+const getAdjacencyList = (
+  edges: {
+    source: string
+    target: string
+  }[],
+) => {
+  const adjacencyList = new Map<string, string[]>()
+  edges.forEach(({ source, target }) => {
+    const valueOfSource = adjacencyList.get(source) || []
+    valueOfSource.push(target)
+    adjacencyList.set(source, valueOfSource)
+  })
+
+  return adjacencyList
+}
+
+export const getCycle = (
+  edges: {
+    source: string
+    target: string
+  }[],
+) => {
+  const adjacencyList = getAdjacencyList(edges)
+  let queue = Array.from(adjacencyList.keys()).map((node) => [node])
+  while (queue.length) {
+    const batch = []
+    for (const path of queue) {
+      const parents = adjacencyList.get(path[0]) || []
+      for (const node of parents) {
+        if (node === path[path.length - 1]) return [node, ...path]
+        batch.push([node, ...path])
+      }
+    }
+    queue = batch
+  }
+}
