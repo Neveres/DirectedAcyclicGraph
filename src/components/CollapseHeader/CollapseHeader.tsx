0aaa2f0b2dbfb51f2d12924026e6e0dd53d03052
Leo <lechewu@gmail.com>
2023-04-27 15:05:15 +0800

    feat: update hints

--- src/components/CollapseHeader/CollapseHeader.tsx
+++ src/components/CollapseHeader/CollapseHeader.tsx
@@ -6,11 +6,12 @@ const { Panel } = Collapse
 const hints = [
   'To add nodes, hold shift and click on the grid.',
   'To add edges, hold shift and click/drag to between nodes.',
-  'To delete a node or edge, click on it and press delete.',
+  'To delete a node or edge, click on it and press Del button on your keyboard.',
   'Click and drag nodes to change their position.',
+  'If you want to create the edge that deleted before, then you should remove node first and create both node and edge again.',
 ]
 
-const NOTE = 'Note: In MacOS, use command instead of Ctrl.'
+const NOTE = 'Note: For macOS, use command instead of Ctrl.'
 
 const Hints = () => (
   <ul>
